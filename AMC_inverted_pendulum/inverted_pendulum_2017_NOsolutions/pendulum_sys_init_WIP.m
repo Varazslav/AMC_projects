a70c147339c5d7cdc7049c3334960157bf7eac50
Varazslav <Varazslav@gmail.com>
2019-05-07 19:19:18 +0200

    names changed and added some minor modification to simulink file

--- AMC_inverted_pendulum/inverted_pendulum_2017_NOsolutions/pendulum_sys_init_WIP.m
+++ AMC_inverted_pendulum/inverted_pendulum_2017_NOsolutions/pendulum_sys_init_WIP.m
@@ -3,17 +3,8 @@ close all
 clc
 
 M=10;   %mass of the cart          [kg]
-m=2;    %mass of the pendulum      [kg]
-l=1;    %lenght                    [m]
-ra=10;  %armature resistance       [ohm]
-rr=.1;  %radius of the wheels      [m]
-ke=2;   %constant                  [N*m/A]
-kc=2;   %constant                  [V*s/rad]
-g=9.81;
-
-M=10;   %mass of the cart          [kg]
-m=2;    %mass of the pendulum      [kg]
-l=1;    %lenght                    [m]
+m=0.5;    %mass of the pendulum      [kg]
+l=1.5;    %lenght                    [m]
 ra=10;  %armature resistance       [ohm]
 rr=.1;  %radius of the wheels      [m]
 ke=2;   %constant                  [N*m/A]
@@ -23,12 +14,12 @@ g=9.81;
 %% Time based linearization
 
 % Run simulink openloop
-sim("pendulum_openloop_anim", 1);
+sim("pendulum_openloop_anim_base", 1);
 
-A = pendulum_openloop_anim_Timed_Based_Linearization.a;
-B = pendulum_openloop_anim_Timed_Based_Linearization.b;
-C = pendulum_openloop_anim_Timed_Based_Linearization.c;
-D = pendulum_openloop_anim_Timed_Based_Linearization.d;
+A = pendulum_openloop_anim_base_Timed_Based_Linearization.a;
+B = pendulum_openloop_anim_base_Timed_Based_Linearization.b;
+C = pendulum_openloop_anim_base_Timed_Based_Linearization.c;
+D = pendulum_openloop_anim_base_Timed_Based_Linearization.d;
 
 
 poles = eig(A)
